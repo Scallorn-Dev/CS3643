fc5197c2c2b8f272fc61c228d56dca68bce1ff5b
Scallorn-dev <mattscallorn@gmail.com>
2025-03-11 12:35:38 -0500

    module 2 lab 1: Simple equation

---
+++ Assignment2/lab2.1a.py
@@ -0,0 +1,6 @@
+'''
+Scenario
+In this challenge, we will try to get conversant with the order of arithmetic operators in Python. Rewrite the following equation as a Python expression and get the result of the equation: 5(4-2)+(100/5/2)2
+'''
+equation = 5 * (4-2) + (100/(5/2)) * 2
+print(equation)  
